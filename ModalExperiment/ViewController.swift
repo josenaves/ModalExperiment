f4a281ee7c067089cfe366c33ae79d8f19014ab3
José Naves <josenaves@gmail.com>
2018-09-24 11:34:27 -0300

    Add IBActions

--- ModalExperiment/ViewController.swift
+++ ModalExperiment/ViewController.swift
@@ -15,6 +15,29 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func experimentImagePicker() {
+        let nextController = UIImagePickerController()
+        present(nextController, animated: true, completion: nil)
+    }
+    
+    @IBAction func experimentActivityView() {
+        let image = UIImage()
+        let controller = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        self.present(controller, animated: true, completion: nil)
+    }
+    
+    @IBAction func experimentAlertController() {
+        let controller = UIAlertController()
+        controller.title = "Test Alert"
+        controller.message = "This is a test"
+        
+        let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) {
+            action in self.dismiss(animated: true, completion: nil)
+        }
+        
+        controller.addAction(okAction)
+        self.present(controller, animated: true, completion: nil)
+    }
 
 }
 
